07a4661694acda2d3659165d02e7f9109338450e
Shaun Stanley <shaun.stanley_ug23@ashoka.edu.in>
2022-12-18 19:06:49 +0530

    Solve Q_01

--- HW 06/Q_01.py
+++ HW 06/Q_01.py
@@ -1,12 +1,22 @@
-def remove_even_and_print(numbers):
-    # Create a new list containing only the odd numbers from the input list
-    odd_numbers = [x for x in numbers if x % 2 == 1]
+# Create a function to remove even numbers from a given array
+def removeEvenNumbers(numbers):
+
+    # Create a new empty list and append only the odd numbers from the input list
+    oddNumbers = []
+
+    # Loop through the numbers in the input list
+    for n in numbers:
+      # If number 'n' from the input list is odd, append it to the new list
+        if n % 2 == 1:
+            oddNumbers.append(n)
 
     # Print the odd numbers
-    for number in odd_numbers:
+    for number in oddNumbers:
         print(number)
 
 
-# Test the function
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-remove_even_and_print(numbers)
+# Create a list with both odd and even numbers
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 14, 15]
+
+# Call the function to remove the even numbers and print the odd ones
+removeEvenNumbers(numbers)
